893b0355fffa09bf807df2fc0a7ceabbe5a16323
piti <90437140+pitiflutter@users.noreply.github.com>
2023-04-17 10:15:20 +0330

    test

--- controllers/invoiceController.go
+++ controllers/invoiceController.go
@@ -1,9 +1,15 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
 
 func GetInvoices()gin.HandlerFunc{
-	return func (ctx *gin.Context)  {
+	return func (c *gin.Context)  {
+		ctx ,canecl := context.WithTimeout(context.Background(),100*time.Second)
 		
 	}
 }
